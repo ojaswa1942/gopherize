82f6214ade96f68b81b9b691b5e50f82cabad65f
Ojaswa Sharma <ojaswa1942@gmail.com>
2020-08-17 01:33:35 +0530

    quiz: complete part#2. Modularize Update ReadMe

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Gophercises
+# Gopherize
 
-This repository contains my solutions to Go Coding Exercises by [Jon Calhoun](https://courses.calhoun.io/courses/cor_gophercises).
+Some micro-programs to exercise GoLang
 
-## Questions
+## Contents 
+In order of time of implementation:
 
 ### Quiz Game
+A program to run timed quizes via the command line.
+Inspiration: Go Coding Exercises by [Jon Calhoun](https://courses.calhoun.io/courses/cor_gophercises).
+
--- quiz-game/main.go
+++ quiz-game/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"encoding/csv"
 	"strings"
+	"time"
 )
 
 type problem struct {
@@ -14,36 +15,58 @@ type problem struct {
 }
 
 func main() {
+	// Parse flags
 	csvFilename := flag.String("csv", "problems.csv", "csv file in format of `question,answer`")
+	timeLimit := flag.Int("limit", 10, "quiz time limit in seconds")
 	flag.Parse()
 
+	// Read file & parse questions
+	lines := readCSV(csvFilename)
+	problems := parseQuestions(lines)
+
+	// Init timer & begin quiz
+	timer := time.NewTimer( time.Duration(*timeLimit) * time.Second)
+	score := 0
+
+	for i, problem := range problems {
+		fmt.Printf("Question #%d: %s? ", i+1, problem.ques)
+		userAnswer := getNonBlockingInput()
+
+		select {
+			case <-timer.C:
+				fmt.Printf("\n\nTortoise alert: Time up!\n")
+				exit(fmt.Sprintf("Your score: %d/%d", score, len(problems)), false)
+			case answer := <-userAnswer :
+				if strings.ToLower(answer) == problem.ans {
+					score++
+				}
+		}
+	}
+
+	exit(fmt.Sprintf("\nYour score: %d/%d", score, len(problems)), false)
+}
+
+func getNonBlockingInput() (chan string) {
+	answerChannel := make(chan string)
+	go func () {
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		answerChannel <- answer
+	}()
+	return answerChannel	
+}
+
+func readCSV(csvFilename *string) [][]string {
 	file, err := os.Open(*csvFilename)
 	if err != nil {
-		exit(fmt.Sprintf("Error opening file: %s\n", *csvFilename))
+		exit(fmt.Sprintf("Error opening file: %s\n", *csvFilename), true)
 	}
 	reader := csv.NewReader(file)
 	lines, err := reader.ReadAll()
 	if err != nil {
-		exit("Failed to parse CSV")
+		exit("Failed to parse CSV", true)
 	}
-
-	problems := parseQuestions(lines)
-	score := 0
-
-	for i, problem := range problems {
-		fmt.Printf("Question #%d: %s\n", i+1, problem.ques)
-		var answer string
-		// scanf handles whitespace trimming
-		fmt.Scanf("%s\n", &answer)
-		if answer == problem.ans {
-			score++
-		}
-	}
-
-	fmt.Printf("Your score: %d/%d\n", score, len(problems))
-
-	fmt.Printf("There you go: %s\n", *csvFilename)
-	fmt.Println(problems)
+	return lines
 }
 
 func parseQuestions(questions [][]string) []problem {
@@ -51,13 +74,17 @@ func parseQuestions(questions [][]string) []problem {
 	for i, question := range questions {
 		parsed[i] = problem {
 			ques: question[0],
-			ans: strings.TrimSpace(question[1]),
+			ans: strings.ToLower(strings.TrimSpace(question[1])),
 		}
 	}
 	return parsed
 }
 
-func exit(msg string) {
+func exit(msg string, error bool) {
 	fmt.Println(msg)
-	os.Exit(1)
+	if error {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
+	}
 }
