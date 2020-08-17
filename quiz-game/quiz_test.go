864624169c6b28593c1a076b86de73c1fd2a3c80
Ojaswa Sharma <ojaswa1942@gmail.com>
2020-08-18 03:42:36 +0530

    quiz: refactor & complete unit tests

--- quiz-game/quiz_test.go
+++ quiz-game/quiz_test.go
@@ -1,63 +1,23 @@
 package main
 
 import (
-	// "strings"
-	// "fmt"
+	"flag"
 	"testing"
-	// "time"
+	"os"
 )
 
-// func testEachQuestion(t *testing.T) {
-// 	timer := time.NewTimer(time.Duration(2) * time.Second).C
-// 	done := make(chan string)
-// 	var quest Question
-// 	quest.question = "1+1"
-// 	quest.answer = "2"
-// 	var ans int
-// 	var err error
-// 	allDone := make(chan bool)
-// 	go func() {
-// 		ans, err = eachQuestion(quest.question, quest.answer, timer, done)
-// 		allDone <- true
-// 	}()
-// 	done <- "2"
+var csvFilename = flag.String("csv", "problems.csv", "csv file in format of `question,answer`")
 
-// 	<-allDone
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	assert.Equal(t, ans, 1)
-// }
-
-// func testReadCSV(t *testing.T) {
-// 	str := "1+1,2\n2+1,3\n9+9,18\n"
-// 	quest, err := readCSV(strings.NewReader(str))
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	var que [3]Question
-// 	que[0].answer = "2"
-// 	que[1].answer = "3"
-// 	que[2].answer = "18"
-// 	que[0].question = "1+1"
-// 	que[1].question = "2+1"
-// 	que[2].question = "9+9"
-
-// 	assert.Equal(t, que[0], quest[0])
-// 	assert.Equal(t, que[1], quest[1])
-// 	assert.Equal(t, que[2], quest[2])
-
-// }
-
-// func TestEachQuestion(t *testing.T) {
-// 	t.Run("test eachQuestion", testEachQuestion)
-// }
+func TestMain(m *testing.M) {
+	flag.Parse()
+	exitResponse := m.Run()
+	os.Exit(exitResponse)
+}
 
 func TestCSV(t *testing.T) {
 	// t.Run("test ReadCSV", testReadCSV)
 	t.Log("using default file problems.csv to conduct test")
-	csvFilename := "problems.csv"
-	lines := readCSV(&csvFilename)
+	lines := readCSV(csvFilename)
 
 	t.Run("check number of lines", func (t *testing.T) {
 		if i := len(lines); i <= 0 {
@@ -102,8 +62,8 @@ func TestScore(t *testing.T) {
 	}{
 		{ []string{"4", "world", "33", "11", "haha", "25"}, 6 },
 		{ []string{"4", "world", "44"}, 2 },
-		{ []string{"4", "world", "44"}, 2 },
-		{ []string{"4", "world", "44"}, 2 },
+		{ []string{"2", "wor", "44"}, 0 },
+		{ []string{"4", "world", "44", "11", "yahoo"}, 3 },
 	};
 
 	t.Log("test all sets of solution")
@@ -120,5 +80,25 @@ func TestScore(t *testing.T) {
 	}
 }
 
-func TestTimer(t *testing.T) {
+func TestStringSanitize(t *testing.T) {
+	cases := []struct{
+		input, output string
+	}{
+		{ "", "" },
+		{ "  haha", "haha" },
+		{ "  ha ha", "ha ha" },
+		{ "  ha ha   ", "ha ha" },
+		{ "  haha   ", "haha" },
+		{ "  h a YAYA h a   ", "h a yaya h a" },
+		{ "  CAPS", "caps" },
+		{ "  CAPSYaAa", "capsyaaa" },
+		{ "  CAPS 69  ", "caps 69" },
+	}
+
+	t.Log("initiate testing trim and caps cases")
+	for i, testcase := range cases {
+		if generated, expected := sanitizeString(testcase.input), testcase.output; generated != expected {
+			t.Errorf("case #%d failed. expected: `%s`, received: `%s`", i+1, expected, generated)
+		}
+	}
 }
