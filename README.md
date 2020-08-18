6eac5e04a3757b30de32178fb44fec593fe114f1
Ojaswa Sharma <ojaswa1942@gmail.com>
2020-08-19 00:27:28 +0530

    urlshortener: create yaml & map parse functions update readme

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Some micro-programs to exercise GoLang
 In order of time of implementation:
 
 ### Quiz Game
-A program to run timed quizes via the command line.
+A program to run timed quizes via the command line (with unit tests).
+Inspiration: Go Coding Exercises by [Jon Calhoun](https://courses.calhoun.io/courses/cor_gophercises).
+
+### URL Shortener
+A program (package) that forwards paths to other URLs (similar to Bitly).
+package `main` depicts a sample usage of the created package `urlshortener`
 Inspiration: Go Coding Exercises by [Jon Calhoun](https://courses.calhoun.io/courses/cor_gophercises).
 
---
+++ urlshortener/urlshortener.go
@@ -0,0 +1,52 @@
+package urlshortener
+
+import (
+	"net/http"
+	"gopkg.in/yaml.v2"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	handlerFn := func (w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if elem, okay := pathsToUrls[path]; okay {
+			http.Redirect(w, r, elem, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+
+	return handlerFn;
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYml(yml)
+	if err != nil {
+		return nil, err
+	}
+
+	pathsToUrls := createMap(pathUrls)
+	
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func parseYml(yml []byte) ([]path, error) {
+	var pathUrls []path
+	if err := yaml.Unmarshal(yml, &pathUrls); err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+func createMap(pathUrls []path) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pathUrl := range pathUrls {
+		pathsToUrls[pathUrl.Path] = pathUrl.URL
+	}
+	return pathsToUrls
+}
+
+// Struct fields are only unmarshalled if they are exported
+type path struct {
+	Path string `yaml:"path"`
+	URL string `yaml:"url"`
+}
